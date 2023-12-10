88b1152d7fddce9946fbeea9c1931be933fe6719
tinghaolai <analth355@gmail.com>
2023-12-10 13:19:23 +0800

    Feat: add navigation

--- src/app/test-page/page.tsx
+++ src/app/test-page/page.tsx
@@ -1,3 +1,16 @@
+"use client";
+import { useRouter } from "next/navigation"
+
 export default function TestPage() {
-    return <h1>TestPage</h1>
+    const router = useRouter()
+    const toTestIdPage = () => {
+        router.push("/test-page/55")
+    }
+
+    return (
+        <div>
+            <h1>TestPage</h1>
+            <button onClick={toTestIdPage}>to test id page</button>
+        </div>
+    )
 }
