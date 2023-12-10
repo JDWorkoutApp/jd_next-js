1748b3444a4c276e2b7baec4dfc82f213055d7ec
tinghaolai <analth355@gmail.com>
2023-12-10 17:59:04 +0800

    Feat: delete api call self and env

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,5 +1,7 @@
 import { NextResponse } from 'next/server';
 
+const TEST_ENV_NAME: string = process.env.TEST_ENV_NAME as string
+
 export async function GET() {
     const testData: Test[] =[
         {
@@ -13,4 +15,23 @@ export async function GET() {
     ]
 
     return NextResponse.json(testData);
+}
+
+export async function DELETE(request: Request) {
+    const { name }: { name: string } = await request.json();
+
+    const res = await fetch(`http://localhost:3000/api/test`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    });
+
+    const data: Test[] = await res.json();
+
+    return NextResponse.json({
+        message: `Deleted ${name}`,
+        env: TEST_ENV_NAME,
+        testApiData: data
+    });
 }
