a0f5eac636612d9310f24571ed6e5e8f1fa92072
tinghaolai <analth355@gmail.com>
2023-12-10 13:01:03 +0800

    Feat: group pages

---
+++ src/app/test-page/(test-route-group)/group-a/page.tsx
@@ -0,0 +1,4 @@
+export default function GroupA() {
+    // http://localhost:3000/test-page/group-a
+    return <h1>Group A page</h1>
+}
---
+++ src/app/test-page/(test-route-group)/group-b/page.tsx
@@ -0,0 +1,4 @@
+export default function GroupA() {
+    // http://localhost:3000/test-page/group-b
+    return <h1>Group A page</h1>
+}
